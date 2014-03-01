70ba5cf21904820e027ab00280134d6b98eccbd7
md_5 <git@md-5.net>
2014-03-02 08:49:48 +1100

    Add both licenses

--- package/libvirt/libvirt.mk
+++ package/libvirt/libvirt.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-LIBVIRT_VERSION           = 1.2.1
-LIBVIRT_SOURCE            = libvirt-$(LIBVIRT_VERSION).tar.gz
-LIBVIRT_SITE              = http://libvirt.org/sources/
-LIBVIRT_LICENSE           = LGPLv2.1+
-LIBVIRT_LICENSE_FILES     = COPYING.LIB
-# Note: both COPYING and COPYING.LIB are present, but they both contain
-#       the text of LGPLv2.1, so we only reference one of them.
-LIBVIRT_DEPENDENCIES       = host-pkgconf lvm2 libnl libxml2 yajl
+LIBVIRT_VERSION       = 1.2.1
+LIBVIRT_SOURCE        = libvirt-$(LIBVIRT_VERSION).tar.gz
+LIBVIRT_SITE          = http://libvirt.org/sources/
+LIBVIRT_LICENSE       = GPLv2 LGPLv2.1
+LIBVIRT_LICENSE_FILES = COPYING COPYING.LESSER
+LIBVIRT_DEPENDENCIES  = host-pkgconf lvm2 libnl libxml2 yajl
 
 $(eval $(autotools-package))
